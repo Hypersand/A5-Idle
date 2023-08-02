48f1dbcb3c5c2df95cf7ddfe3ca9378c0fdc6fa7
jjun990908 <jjun990908@naver.com>
2023-08-02 15:21:34 +0900

    [init] font 및 color 초기 세팅

--- frontend/Idle/src/App.jsx
+++ frontend/Idle/src/App.jsx
@@ -1,7 +1,15 @@
-import "./App.css";
+import { ThemeProvider } from "styled-components";
+import color from "../styles/theme";
+import { Reset } from "../styles/GlobalStyle";
+import Title from "./components/common/title";
 
 function App() {
-  return null;
+  return (
+    <ThemeProvider theme={color}>
+      <Reset />
+        <Title>aisdvhljnddsaㅏㄴㅇ루파ㅣㅓㄴ욾</Title>
+    </ThemeProvider>
+  )
 }
 
 export default App;
---
+++ frontend/Idle/utils/routes.jsx
@@ -0,0 +1,2 @@
+export const FONT_HEAD = "frontend/Idle/src/assets/fonts/AnyConv.com__HyundaiSansHeadKRRegular.woff"
+export const FONT_TEXT = "frontend/Idle/src/assets/fonts/AnyConv.com__HyundaiSansTextKRRegular.woff"
