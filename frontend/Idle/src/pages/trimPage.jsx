f41d13e3fe3a82a50b1096a0a5ff9c51fd83eaf7
sungho <shson1217@kw.ac.kr>
2023-08-03 16:36:53 +0900

    [feat] 확인버튼 누를 시 차객체를비움

--- frontend/Idle/src/pages/trimPage.jsx
+++ frontend/Idle/src/pages/trimPage.jsx
@@ -1,12 +1,10 @@
 import MainLogo from "../components/common/MainLogo.jsx";
 import CarNameDropdown from "../components/carNameDropdown/carNameDropdown.jsx";
-import WarningModal from "../components/common/warningModal.jsx";
 function TrimPage() {
   return (
     <div>
       <MainLogo />
       <CarNameDropdown />
-      <WarningModal title={"차량을 선택해주세요."} />
       {/* trimPage */}
     </div>
   );
