4223d013b0e48fcf96c3670eb0552e4e7a6d5bd7
jjun990908 <jjun990908@naver.com>
2023-08-04 17:42:46 +0900

    [feat] 트림 선택 페이지 구현

--- frontend/Idle/src/pages/trimPage.jsx
+++ frontend/Idle/src/pages/trimPage.jsx
@@ -1,22 +1,101 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getTrimData } from "../utils/api";
 import TrimBoxContainer from "../components/TrimBoxContainer/TrimBoxContainer";
-// import { styled } from "styled-components";
+import { styled } from "styled-components";
+import BlueButton from "../components/common/BlueButton";
+import { useNavigate } from "react-router-dom";
+import { TRIM_ROUTE } from "../utils/routes";
+import { carContext } from "../utils/context";
+import FindTrim from "../components/findTrim/FindTrim";
 
 function TrimPage() {
+  const { car } = useContext(carContext);
+  const navigate = useNavigate();
   const [trimData, setTrimData] = useState(null);
 
+  function nextBTNClicked() {
+    navigate("/detail");
+  }
   useEffect(() => {
     getTrimData().then((result) => {
       setTrimData(result);
     });
   }, []);
 
-  return <div>{trimData ? <TrimBoxContainer {...trimData} /> : <p>Loading...</p>}</div>;
+  return (
+    <>
+      <StImageContainer src={`${TRIM_ROUTE}${car.trim.name}.png`} />
+      <StBottomContainer>
+        {trimData ? <TrimBoxContainer {...trimData} /> : <p>Loading...</p>}
+        <StConfirmContainer>
+          <StConfirmHeader>
+            <Title>트림 선택</Title>
+            <Description>원하는 트림을 선택해주세요.</Description>
+          </StConfirmHeader>
+          <BlueButton text={"다음"} onClick={nextBTNClicked} />
+        </StConfirmContainer>
+      </StBottomContainer>
+      <TrimSelectContainer>
+        <FindTrim />
+      </TrimSelectContainer>
+    </>
+  );
 }
 
 export default TrimPage;
 
-// const StBottomContainer = styled.div`
+const StImageContainer = styled.img`
+  position: absolute;
+  top: 120px;
+  left: 130px;
+`;
+const StBottomContainer = styled.div`
+  position: absolute;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 46px;
+  bottom: 64px;
+  left: 128px;
+`;
 
-// `
+const StConfirmContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  gap: 78px;
+`;
+
+const StConfirmHeader = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 4px;
+`;
+const Title = styled.h1`
+  color: #222;
+  font-family: Hyundai Sans Text KR;
+  font-size: 16px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: 24px;
+  letter-spacing: -0.48px;
+`;
+const Description = styled.p`
+  color: #222;
+  font-family: Hyundai Sans Text KR;
+  font-size: 13px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: 165%;
+  letter-spacing: -0.39px;
+`;
+
+const TrimSelectContainer = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: end;
+  bottom: 18px;
+  left: 585px;
+`;
