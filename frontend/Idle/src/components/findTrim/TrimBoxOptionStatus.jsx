10b22669048d1c72d6bcd9d01e35201989a3fefa
sungho <shson1217@kw.ac.kr>
2023-08-07 13:01:57 +0900

    [add] 커스텀 컴포넌트 컨테이너추가

---
+++ frontend/Idle/src/components/findTrim/TrimBoxOptionStatus.jsx
@@ -0,0 +1,44 @@
+import styled from "styled-components";
+
+function TrimBoxOptionStatus({ status }) {
+  return (
+    <div>
+      {status === "add" ? (
+        <StTrimBoxAddOption>추가옵션</StTrimBoxAddOption>
+      ) : (
+        <StTrimBoxDefaultOption>기본옵션</StTrimBoxDefaultOption>
+      )}
+    </div>
+  );
+}
+
+export default TrimBoxOptionStatus;
+
+const StTrimBoxAddOption = styled.div`
+  width: 52px;
+  height: 20px;
+  padding-top: 3px;
+  background-color: ${({ theme }) => theme.NavyBlue_1};
+  color: ${({ theme }) => theme.NavyBlue_5};
+  font-family: Hyundai Sans Text KR;
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 16px;
+  letter-spacing: -0.03em;
+  text-align: center;
+  justify-content: center;
+`;
+
+const StTrimBoxDefaultOption = styled.div`
+  width: 52px;
+  height: 20px;
+  padding-top: 3px;
+  background-color: ${({ theme }) => theme.NavyBlue_5};
+  color: ${({ theme }) => theme.NavyBlue_1};
+  font-family: Hyundai Sans Text KR;
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 16px;
+  letter-spacing: -0.03em;
+  text-align: center;
+`;
