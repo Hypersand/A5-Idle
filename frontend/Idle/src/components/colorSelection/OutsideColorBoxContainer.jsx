b6fcb867e6757bce7218392657497b8992a7c704
WongilK <wwwkim99@naver.com>
2023-08-09 14:16:43 +0900

    [feat] 외장 색상 컨테이너 연결

--- frontend/Idle/src/components/colorSelection/OutsideColorBoxContainer.jsx
+++ frontend/Idle/src/components/colorSelection/OutsideColorBoxContainer.jsx
@@ -1,73 +1,19 @@
 import { styled } from "styled-components";
 import OutsideColorBox from "../common/boxs/OutsideColorBox";
-import { useState } from "react";
-import grey from "../../assets/images/outsideColor/grey.png";
-import black from "../../assets/images/outsideColor/black.png";
-import white from "../../assets/images/outsideColor/white.png";
-import silver from "../../assets/images/outsideColor/silver.png";
-import brown from "../../assets/images/outsideColor/brown.png";
-import emerald from "../../assets/images/outsideColor/emerald.png";
 
-const data = [
-  {
-    exterior_id: 1,
-    exterior_name: "grey",
-    exterior_price: 0,
-    exterior_img_url: grey,
-    exteriror_purchase_rate: "구매자의 32%가 선택",
-  },
-  {
-    exterior_id: 15,
-    exterior_name: "black",
-    exterior_price: 0,
-    exterior_img_url: black,
-    exteriror_purchase_rate: "구매자의 32%가 선택",
-  },
-  {
-    exterior_id: 3,
-    exterior_name: "white",
-    exterior_price: 0,
-    exterior_img_url: white,
-    exteriror_purchase_rate: "구매자의 32%가 선택",
-  },
-  {
-    exterior_id: 31,
-    exterior_name: "brown",
-    exterior_price: 0,
-    exterior_img_url: brown,
-    exteriror_purchase_rate: "구매자의 32%가 선택",
-  },
-  {
-    exterior_id: 5,
-    exterior_name: "silver",
-    exterior_price: 0,
-    exterior_img_url: silver,
-    exteriror_purchase_rate: "구매자의 32%가 선택",
-  },
-  {
-    exterior_id: 333,
-    exterior_name: "emerald",
-    exterior_price: 0,
-    exterior_img_url: emerald,
-    exteriror_purchase_rate: "구매자의 32%가 선택",
-  },
-];
-
-function OutsideColorBoxContainer() {
-  /*상위 컴포넌트에서 정의하고 받아오기 */
-  const [selectedColor, setSelectedColor] = useState("");
-  function boxClicked(color) {
-    setSelectedColor(color);
+function OutsideColorBoxContainer({ data, selectedColor, setSelectedColor }) {
+  function boxClicked(name, price) {
+    setSelectedColor({ name: name, price: price });
   }
 
   function renderBox() {
     return data.map((item) => {
       return (
         <OutsideColorBox
-          onClick={() => boxClicked(item.exterior_name)}
+          onClick={() => boxClicked(item.exterior_name, item.exterior_price)}
           key={item.exterior_id}
           data={item}
-          isSelected={selectedColor === item.exterior_name}
+          isSelected={selectedColor.name === item.exterior_name}
         />
       );
     });
