d230ff58ee213675ce2b9d81f3c4c37b6af9c687
sungho <shson1217@kw.ac.kr>
2023-08-10 13:59:49 +0900

    [feat] 기본옵션보기 컨테이너

--- frontend/Idle/src/components/defaultOption/Modal.jsx
+++ frontend/Idle/src/components/defaultOption/Modal.jsx
@@ -3,12 +3,22 @@ import { useState } from "react";
 
 function Modal() {
   const [animationstate, setAnimationState] = useState(false);
-  return <StContainer />;
+  return (
+    <StContainer>
+      <StCloseButtonContainer></StCloseButtonContainer>
+      <StCategoryContainer> </StCategoryContainer>
+      <StContentContainer></StContentContainer>
+      <StPaginationContainer></StPaginationContainer>
+    </StContainer>
+  );
 }
 
 export default Modal;
 
 const StContainer = styled.div`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
   position: absolute;
   width: 1280px;
   height: 692.0703125px;
@@ -28,4 +38,45 @@ const StContainer = styled.div`
     opacity: 1;
   }
   `} 1s ease-in-out;
+`;
+
+const StCloseButtonContainer = styled.div`
+  width: 1032px;
+  height: 54px;
+  flex-shrink: 0;
+  align-items: center;
+  border: 3px solid rgba(255, 122, 0, 0.3);
+  margin-top: 6.07px;
+`;
+
+const StCategoryContainer = styled.div`
+  display: flex;
+  width: 1024px;
+  height: 30px;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid black;
+  margin-top: 17.93px;
+`;
+
+const StContentContainer = styled.div`
+  display: inline-flex;
+  flex-direction: column;
+  align-items: flex-start;
+  width: 1024px;``
+  height: 456px;
+  gap: 32px;
+  border: 1px solid black;
+  margin-top: 22.07px;
+`;
+
+const StPaginationContainer = styled.div`
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  width: 107px;
+  height: 24px;
+  gap: 32px;
+  margin-top: 34px;
+  border: 1px solid black;
 `;
