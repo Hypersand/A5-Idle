1170402d60f0c844ae69fd68d390011bf8a55576
Jeongyeon Kim <wjddus9132@gmail.com>
2023-08-10 20:38:44 +0900

    [test] 외장 색상 controller 테스트

--- backend/src/test/java/com/autoever/idle/domain/exteriorColor/ExteriorColorRepositoryTest.java
+++ backend/src/test/java/com/autoever/idle/domain/exteriorColor/ExteriorColorRepositoryTest.java
@@ -12,7 +12,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-@DisplayName("Exterior Color Repositor Test")
+@DisplayName("Exterior Color Repository Test")
 class ExteriorColorRepositoryTest {
 
     @Autowired
