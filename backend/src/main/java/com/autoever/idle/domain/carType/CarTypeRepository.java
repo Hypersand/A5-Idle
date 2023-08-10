ab993153a16bf0e47df8367336f5a703e706a480
Jeongyeon Kim <wjddus9132@gmail.com>
2023-08-10 13:18:31 +0900

    [refactor] repository interface에서 @repository 어노테이션 삭제

--- backend/src/main/java/com/autoever/idle/domain/carType/CarTypeRepository.java
+++ backend/src/main/java/com/autoever/idle/domain/carType/CarTypeRepository.java
@@ -1,11 +1,7 @@
 package com.autoever.idle.domain.carType;
 
-
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 
-@Repository
 public interface CarTypeRepository{
     List<Long> findByName(String carTypeName);
 }
