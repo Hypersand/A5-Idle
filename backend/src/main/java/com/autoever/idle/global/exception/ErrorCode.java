bd0e59d067ca82c434abdcda972b19d6eea7720e
Hypersand <on8214@gmail.com>
2023-08-10 22:09:43 +0900

    [fix] ErrorCode 코드 오류 수정

--- backend/src/main/java/com/autoever/idle/global/exception/ErrorCode.java
+++ backend/src/main/java/com/autoever/idle/global/exception/ErrorCode.java
@@ -4,14 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-
-
     INVALID_CAR(400, "존재하지 않는 차종입니다"),
 
-    INVALID_DETAIL_MODEL(400, "존재하지 않는 모델입니다"),
-
-    INVALID_CAR(400, "존재하지 않는 차종입니다");
-
+    INVALID_DETAIL_MODEL(400, "존재하지 않는 모델입니다");
 
     private final int status;
     private final String message;
@@ -21,4 +16,4 @@ public enum ErrorCode {
         this.message = message;
     }
 
-}
+    }
