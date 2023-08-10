b822d98c33afbc36b10814e5eca9a4745c1f6cfb
sungho <shson1217@kw.ac.kr>
2023-08-10 13:20:49 +0900

    [feature] 기본옵션보기 모달 애니메이션

--- frontend/Idle/src/components/defaultOption/Modal.jsx
+++ frontend/Idle/src/components/defaultOption/Modal.jsx
@@ -1,6 +1,8 @@
-import styled from "styled-components";
+import { styled, keyframes } from "styled-components";
+import { useState } from "react";
 
 function Modal() {
+  const [animationstate, setAnimationState] = useState(false);
   return <StContainer />;
 }
 
@@ -11,5 +13,19 @@ const StContainer = styled.div`
   width: 1280px;
   height: 692.0703125px;
   top: 28px;
-  border: 1px black solid;
+  background-color: ${({ theme }) => theme.Grey_1};
+  transition:
+    transform 1s ease-in-out,
+    opacity 1s ease-in-out;
+  ${({ $animationstate }) => $animationstate === true && "transform: translateY(20%); opacity: 0;"}
+  animation: ${keyframes`
+  0% {
+    transform: translateY(20%);
+    opacity: 0;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  `} 1s ease-in-out;
 `;
