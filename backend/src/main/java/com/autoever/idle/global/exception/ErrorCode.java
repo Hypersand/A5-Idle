ae206643e7c2cbaa10bbe988c638b3400e16939a
Jeongyeon Kim <wjddus9132@gmail.com>
2023-08-10 13:04:44 +0900

    [feat] 유효하지 않은 차종에 대한 에러 처리

---
+++ backend/src/main/java/com/autoever/idle/global/exception/ErrorCode.java
@@ -0,0 +1,18 @@
+package com.autoever.idle.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorCode {
+
+    INVALID_CAR(400, "존재하지 않는 차종입니다");
+
+    private final int status;
+    private final String message;
+
+    ErrorCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+}
---
+++ backend/src/main/java/com/autoever/idle/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,18 @@
+package com.autoever.idle.global.exception;
+
+import com.autoever.idle.global.exception.custom.InvalidCarException;
+import com.autoever.idle.global.exception.dto.ErrorDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler({ InvalidCarException.class })
+    protected ResponseEntity<ErrorDto> handleInvalidCarException(InvalidCarException e) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ErrorDto(e.getErrorCode().getStatus(), e.getErrorCode().getMessage()));
+    }
+}
