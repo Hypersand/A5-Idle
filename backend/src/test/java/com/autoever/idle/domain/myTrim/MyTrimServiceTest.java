fd0e6643ec9ec70dc034f5071aa008c88b727f98
sue06004 <64758888+sue06004@users.noreply.github.com>
2023-08-11 23:12:14 +0900

    [refactor] MyTrimService Mockito를 사용하여 단위테스트로 리팩토링

--- backend/src/test/java/com/autoever/idle/domain/myTrim/MyTrimServiceTest.java
+++ backend/src/test/java/com/autoever/idle/domain/myTrim/MyTrimServiceTest.java
@@ -1,36 +1,56 @@
 package com.autoever.idle.domain.myTrim;
 
+import com.autoever.idle.domain.function.FunctionRepository;
+import com.autoever.idle.domain.function.dto.MyTrimFunctionDto;
 import com.autoever.idle.domain.function.dto.MyTrimFunctionResDto;
+import org.assertj.core.api.SoftAssertions;
+import org.assertj.core.api.junit.jupiter.InjectSoftAssertions;
+import org.assertj.core.api.junit.jupiter.SoftAssertionsExtension;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+@ExtendWith(SoftAssertionsExtension.class)
 class MyTrimServiceTest {
 
-    @Autowired
-    MyTrimService myTrimService;
+    @InjectSoftAssertions
+    private SoftAssertions softly;
+
+    @Mock
+    private FunctionRepository functionRepository;
+
+    @InjectMocks
+    private MyTrimService myTrimService;
 
     @Test
     @DisplayName("내게 맞는 트림 찾기에 들어갈 기능 9가지 반환 메소드")
     void findMyTrimFunctions(){
+        //given
+        List<MyTrimFunctionDto> myTrimFunctionDtoList = new ArrayList<>();
+        myTrimFunctionDtoList.add(new MyTrimFunctionDto(1, "기능1","설명","imgUrl",1));
+        myTrimFunctionDtoList.add(new MyTrimFunctionDto(2, "기능2","설명","imgUrl",1));
+        myTrimFunctionDtoList.add(new MyTrimFunctionDto(4, "기능4","설명","imgUrl",1));
+        when(functionRepository.findMyTrimFunctions()).thenReturn(myTrimFunctionDtoList);
+
+        //when
         List<MyTrimFunctionResDto> myTrimFunctions = myTrimService.findMyTrimFunctions();
 
-        assertThat(myTrimFunctions.size()).isEqualTo(9);
-        assertThat(myTrimFunctions.get(0).getName()).isEqualTo("전후석 통합 터치 공조 컨트롤");
-        assertThat(myTrimFunctions.get(1).getName()).isEqualTo("헤드업 디스플레이");
-        assertThat(myTrimFunctions.get(2).getName()).isEqualTo("냉온장 컵홀더");
-        assertThat(myTrimFunctions.get(3).getName()).isEqualTo("2열 통풍시트");
-        assertThat(myTrimFunctions.get(4).getName()).isEqualTo("캘리그래피 전용 디자인(휠, 라디에이터 그릴 & 가니쉬, 인테이크 그릴, 바디컬러 클래딩, 프론트 & 리어 크롬 스키드 플레이트)");
-        assertThat(myTrimFunctions.get(5).getName()).isEqualTo("후측방 충돌 경고(주행)");
-        assertThat(myTrimFunctions.get(6).getName()).isEqualTo("내비게이션 기반 스마트 크루즈 컨트롤(진출입로)");
-        assertThat(myTrimFunctions.get(7).getName()).isEqualTo("KRELL 프리미엄 사운드(12스피커, 외장앰프)");
-        assertThat(myTrimFunctions.get(8).getName()).isEqualTo("클러스터(12.3인치 컬러 LCD)");
+        //then
+        verify(functionRepository).findMyTrimFunctions();
+        softly.assertThat(myTrimFunctions.size()).isEqualTo(3);
+        softly.assertThat(myTrimFunctions.get(0).getName()).isEqualTo("기능1");
+        softly.assertThat(myTrimFunctions.get(1).getName()).isEqualTo("기능2");
+        softly.assertThat(myTrimFunctions.get(2).getName()).isEqualTo("기능4");
     }
 
 }
