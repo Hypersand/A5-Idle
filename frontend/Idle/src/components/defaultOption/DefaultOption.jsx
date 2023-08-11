b199b657eba87e09d1f97a66a948379420b29d84
sungho <shson1217@kw.ac.kr>
2023-08-11 10:34:32 +0900

    [fix] 버셀오류

--- frontend/Idle/src/components/defaultOption/DefaultOption.jsx
+++ frontend/Idle/src/components/defaultOption/DefaultOption.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import DefaultOptionButton from "./defaultOptionButton";
+import DefaultOptionButton from "./DefaultOptionButton";
 import Modal from "./Modal";
 
 function DefaultOption() {
