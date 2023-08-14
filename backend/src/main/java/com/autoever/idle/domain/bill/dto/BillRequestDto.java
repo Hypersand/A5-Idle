3a2c0d61c4e4cb921aef7fc91c728527b4f4228a
Hypersand <on8214@gmail.com>
2023-08-14 15:46:25 +0900

    [fix] RequestBody -> QueryString으로 요청 변경

--- backend/src/main/java/com/autoever/idle/domain/bill/dto/BillRequestDto.java
+++ backend/src/main/java/com/autoever/idle/domain/bill/dto/BillRequestDto.java
@@ -3,11 +3,13 @@ package com.autoever.idle.domain.bill.dto;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import java.util.List;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 public class BillRequestDto {
     private Long exteriorId;
     private Long interiorId;
