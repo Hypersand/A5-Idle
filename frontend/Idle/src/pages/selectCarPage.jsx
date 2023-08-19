780bff27fc1a6b74ba615db8ad318f6a227b11ae
WongilK <wwwkim99@naver.com>
2023-08-19 22:48:21 +0900

    [fix] console.log 삭제

--- frontend/Idle/src/pages/selectCarPage.jsx
+++ frontend/Idle/src/pages/selectCarPage.jsx
@@ -381,7 +381,6 @@ function SelectCarPage() {
   const tabs = ["수소/전기차", "N", "승용", "SUV", "소형트럭&택시", "트럭", "버스"];
   const navigate = useNavigate();
   const { filteredData, maxPage } = filterData(data, selectedTab, currentPage);
-  console.log(filteredData);
   function renderTabs() {
     return tabs.map((item, index) => {
       return (
