c0ba0023160f625c0d95de03f12b115975499533
sue06004 <64758888+sue06004@users.noreply.github.com>
2023-08-19 12:19:39 +0900

    [refactor] FunctionRepsotiroy RequiredArgsContructor, MapSqlParameter 사용

--- backend/src/main/java/com/autoever/idle/domain/function/repository/FunctionRepositoryImpl.java
+++ backend/src/main/java/com/autoever/idle/domain/function/repository/FunctionRepositoryImpl.java
@@ -4,30 +4,32 @@ import com.autoever.idle.domain.function.dto.FunctionDto;
 import com.autoever.idle.domain.function.dto.MyTrimFunctionDto;
 import com.autoever.idle.domain.myTrim.dto.MyTrimDto;
 import com.autoever.idle.domain.option.dto.MyTrimOptionResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class FunctionRepositoryImpl implements FunctionRepository {
 
-    private final JdbcTemplate jdbcTemplate;
-
-    public FunctionRepositoryImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public List<MyTrimFunctionDto> findMyTrimFunctions() {
         RowMapper rowMapper = BeanPropertyRowMapper.newInstance(MyTrimFunctionDto.class);
         return jdbcTemplate.query("select FUNCTIONS.function_id AS functionId, name, description, trim_id AS trimId, img_url AS imgUrl " +
-                        "from TRIM_FUNCTION TF " +
-                        "join FUNCTIONS on TF.function_id=FUNCTIONS.function_id " +
-                        "where is_my_trim='TRUE' order by trim_id", rowMapper);
+                "from TRIM_FUNCTION TF " +
+                "join FUNCTIONS on TF.function_id=FUNCTIONS.function_id " +
+                "where is_my_trim='TRUE' order by trim_id", rowMapper);
     }
 
     @Override
@@ -36,31 +38,37 @@ public class FunctionRepositoryImpl implements FunctionRepository {
                 "(       SELECT name, is_default, T.trim_id " +
                 "        FROM TRIM_FUNCTION AS TF " +
                 "        JOIN TRIM AS T ON TF.trim_id = T.trim_id " +
-                "        WHERE TF.function_id = ? " +
+                "        WHERE TF.function_id = :functionId " +
                 ") " +
                 "SELECT TRIM.name, is_default AS isDefault FROM TRIM " +
                 "    LEFT JOIN TMP_TRIM_FUNCTION AS TTF " +
                 "    ON TRIM.trim_id=TTF.trim_id";
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("functionId", functionId);
         RowMapper rowMapper = BeanPropertyRowMapper.newInstance(MyTrimDto.class);
-        return jdbcTemplate.query(query, rowMapper, functionId);
+        return jdbcTemplate.query(query, params, rowMapper);
     }
 
     public String checkMyTrimFunction(int functionId) {
+        MapSqlParameterSource param = new MapSqlParameterSource();
+        param.addValue("functionId", functionId);
+        RowMapper<String> rowMapper = new SingleColumnRowMapper<>(String.class);
         try {
-            return jdbcTemplate.queryForObject("SELECT is_my_trim FROM FUNCTIONS WHERE function_id=? ",
-                    ((rs, rowNum) -> rs.getString("is_my_trim")),
-                    functionId);
+            return jdbcTemplate.queryForObject("SELECT is_my_trim FROM FUNCTIONS WHERE function_id=:functionId ", param, rowMapper);
         } catch (EmptyResultDataAccessException e) {
             return null;
         }
     }
+
     @Override
     public MyTrimOptionResponse findOptionBySelectFunction(Long functionId) {
+        MapSqlParameterSource param = new MapSqlParameterSource();
+        param.addValue("functionId", functionId);
         RowMapper rowMapper = BeanPropertyRowMapper.newInstance(MyTrimOptionResponse.class);
         return (MyTrimOptionResponse) jdbcTemplate.queryForObject("SELECT O.option_id AS optionId, O.name AS optionName, price AS optionPrice FROM FUNCTIONS AS F " +
                         "JOIN `OPTION` AS O ON F.option_id=O.option_id " +
-                        "WHERE function_id=?",
-                rowMapper, functionId);
+                        "WHERE function_id=:functionId",
+                param, rowMapper);
     }
 
     @Override
@@ -68,10 +76,12 @@ public class FunctionRepositoryImpl implements FunctionRepository {
         String query = "select f.function_id functionId, f.name functionName, f.description functionDescription, " +
                 "f.img_url functionImgUrl, f.wheel_logo_img_url wheelLogoImgUrl " +
                 "from FUNCTIONS f left join `OPTION` o on f.option_id = o.option_id " +
-                "where f.option_id = ?";
+                "where f.option_id = :optionId";
 
+        MapSqlParameterSource param = new MapSqlParameterSource();
+        param.addValue("optionId", optionId);
         RowMapper rowMapper = new BeanPropertyRowMapper(FunctionDto.class);
-        return jdbcTemplate.query(query, rowMapper, optionId);
+        return jdbcTemplate.query(query, param, rowMapper);
     }
 
 }
