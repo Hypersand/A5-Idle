d4d01e70b2992398288e59191dca7430b7de5b3e
sue06004 <64758888+sue06004@users.noreply.github.com>
2023-08-19 13:03:54 +0900

    [refactor] TrimFunctionRepo RequiredArgs생성자, MapSqlParametr사용

--- backend/src/main/java/com/autoever/idle/domain/function/repository/TrimFunctionRepositoryImpl.java
+++ backend/src/main/java/com/autoever/idle/domain/function/repository/TrimFunctionRepositoryImpl.java
@@ -1,27 +1,31 @@
 package com.autoever.idle.domain.function.repository;
 
 import com.autoever.idle.domain.function.dto.FunctionIdResponse;
+import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class TrimFunctionRepositoryImpl implements TrimFunctionRepository {
 
-    private final JdbcTemplate jdbcTemplate;
-
-    public TrimFunctionRepositoryImpl(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
-    }
+    private final NamedParameterJdbcTemplate jdbcTemplate;
 
     @Override
     public String checkDefaultFunction(Long trimId, Long functionId) {
+        RowMapper<String> rowMapper = new SingleColumnRowMapper<>(String.class);
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("trimId", trimId);
+        params.addValue("functionId", functionId);
         try {
             return jdbcTemplate.queryForObject("SELECT is_default " +
-                            "FROM TRIM_FUNCTION " +
-                            "WHERE trim_id=? AND function_id=?",
-                    (rs, rowNum) -> rs.getString("is_default"),
-                    trimId, functionId);
+                    "FROM TRIM_FUNCTION " +
+                    "WHERE trim_id=:trimId AND function_id=:functionId", params, rowMapper);
         } catch (EmptyResultDataAccessException e) {
             return null;
         }
@@ -29,10 +33,13 @@ public class TrimFunctionRepositoryImpl implements TrimFunctionRepository {
     }
 
     public FunctionIdResponse checkNonSelectableFunctionAtTrim(Long trimId, Long functionId) {
+        RowMapper<Long> rowMapper = new SingleColumnRowMapper<>(Long.class);
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("trimId", trimId);
+        params.addValue("functionId", functionId);
         try {
-            jdbcTemplate.queryForObject("SELECT function_id AS functionId FROM TRIM_FUNCTION WHERE trim_id=? AND function_id=?",
-                    (rs, rowNum) -> rs.getLong("functionId"),
-                    trimId, functionId);
+            jdbcTemplate.queryForObject("SELECT function_id AS functionId FROM TRIM_FUNCTION WHERE trim_id=:trimId AND function_id=:functionId",
+                    params, rowMapper);
             return null;
         } catch (EmptyResultDataAccessException e) {
             return new FunctionIdResponse(functionId);
