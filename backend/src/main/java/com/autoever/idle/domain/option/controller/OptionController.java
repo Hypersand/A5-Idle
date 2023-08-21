d35ab3a042ac495c8fab65a1b4e5df1d3bcf5a13
Hypersand <on8214@gmail.com>
2023-08-21 12:43:36 +0900

    [refactor] 추가 옵션 API 요청 GET 및 쿼리 스트링으로 변경

--- backend/src/main/java/com/autoever/idle/domain/option/controller/OptionController.java
+++ backend/src/main/java/com/autoever/idle/domain/option/controller/OptionController.java
@@ -6,6 +6,7 @@ import com.autoever.idle.domain.option.service.OptionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,11 +20,9 @@ public class OptionController {
 
     private final OptionService optionService;
 
-    @PostMapping("/options")
-    public ResponseEntity<List<OptionFunctionsResponse>> getOptionFunctions(@RequestBody OptionRequest optionRequest) {
+    @GetMapping("/options")
+    public ResponseEntity<List<OptionFunctionsResponse>> getOptionFunctions(OptionRequest optionRequest) {
         List<OptionFunctionsResponse> optionFunctions = optionService.getOptionFunctions(optionRequest);
         return ResponseEntity.ok(optionFunctions);
     }
-
-
 }
