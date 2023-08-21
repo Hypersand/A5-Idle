64fc428b20169e5c3eb5d7d08b5b5d26ccb60be9
Hypersand <on8214@gmail.com>
2023-08-21 11:19:34 +0900

    [refactor] @RequiredArgsConstructor로 변경

--- backend/src/main/java/com/autoever/idle/domain/bill/controller/BillController.java
+++ backend/src/main/java/com/autoever/idle/domain/bill/controller/BillController.java
@@ -3,6 +3,7 @@ package com.autoever.idle.domain.bill.controller;
 import com.autoever.idle.domain.bill.dto.BillRequest;
 import com.autoever.idle.domain.bill.dto.BillResponse;
 import com.autoever.idle.domain.bill.service.BillService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,14 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/result")
 public class BillController {
 
     private final BillService billService;
-
-    public BillController(BillService billService) {
-        this.billService = billService;
-    }
 
     @PostMapping("/bill")
     public ResponseEntity<BillResponse> getResultBill(@RequestBody BillRequest billRequest) {
