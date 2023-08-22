75d561811a049591e9ff2511398b3cd31c824c65
WongilK <wwwkim99@naver.com>
2023-08-22 15:16:47 +0900

    [fix] 경로 설정

--- frontend/Idle/src/components/common/error/ErrorBoundary.jsx
+++ frontend/Idle/src/components/common/error/ErrorBoundary.jsx
@@ -1,33 +1,33 @@
-import { Component } from 'react';
-import ServerErrorPage from '../../../pages/ServerErrorPage';
+import { Component } from "react";
+import ServerErrorPage from "../../../pages/serverErrorPage";
 
 class ErrorBoundary extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            hasError: false,
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.error("Error:", error);
+    console.error("Error Info:", errorInfo);
+  }
+  setError(bool) {
+    return { hasError: bool };
+  }
+  render(setError) {
+    if (this.state.hasError) {
+      // 에러가 발생했을 때 대체 컴포넌트를 반환
+      return <ServerErrorPage setError={setError} />;
     }
 
-    static getDerivedStateFromError(error) {
-        return { hasError: true };
-    }
-
-    componentDidCatch(error, errorInfo) {
-        console.error('Error:', error);
-        console.error('Error Info:', errorInfo);
-    }
-    setError(bool) {
-        return { hasError: bool };
-    }
-    render(setError) {
-        if (this.state.hasError) {
-            // 에러가 발생했을 때 대체 컴포넌트를 반환
-            return <ServerErrorPage setError={setError} />;
-        }
-
-        return this.props.children;
-    }
+    return this.props.children;
+  }
 }
 
 export default ErrorBoundary;
