47263958e7f7f6d1d5fd16ca42f5e8c08a5ca8d1
sungho <shson1217@kw.ac.kr>
2023-08-22 15:48:23 +0900

    [refactor] 필요없는 부분삭제

--- frontend/Idle/src/pages/billPage.jsx
+++ frontend/Idle/src/pages/billPage.jsx
@@ -46,9 +46,9 @@ function BillPage() {
   };
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
     return () => {
-      window.removeEventListener('scroll', handleScroll);
+      window.removeEventListener("scroll", handleScroll);
     };
   }, []);
 
@@ -70,14 +70,10 @@ function BillPage() {
         exteriorId: car.color.exterior.exteriorId,
         interiorId: car.color.interior.interiorId,
         selectedOptionIds: additionalOptionIds,
-      })
-        .then((result) => {
-          setBillData(result);
-          cachedBillData = result;
-        })
-        .catch((error) => {
-          if (error) return <ServerErrorPage />;
-        });
+      }).then((result) => {
+        setBillData(result);
+        cachedBillData = result;
+      });
     }
   }, []);
   function scrollTop() {
@@ -229,16 +225,16 @@ const CarContainer = styled.div`
 `;
 const StConfirmContainer = styled.div`
   display: flex;
-  position:absolute;
+  position: absolute;
   flex-direction: row;
-  top:420px;
+  top: 420px;
   right: 100px;
   gap: 40px;
-    justify-content: center;
-    align-items: flex-end;
+  justify-content: center;
+  align-items: flex-end;
 `;
 const fixedAnimation = css`
-  position:fixed;
+  position: fixed;
   flex-direction: column;
   right: 60px;
   bottom: 120px;
@@ -247,7 +243,7 @@ const fixedAnimation = css`
   padding: 10px 20px 10px 20px;
   border: 1px ridge #dde4f8;
   background-color: white;
-`
+`;
 const StConfirmText = styled.div`
   display: flex;
   flex-direction: column;
@@ -256,8 +252,7 @@ const StConfirmText = styled.div`
   gap: 8px;
   right: 320px;
   cursor: pointer;
-  ${({ $scrollPosition }) =>
-    $scrollPosition > 500 ? fixedAnimation : ""};
+  ${({ $scrollPosition }) => ($scrollPosition > 500 ? fixedAnimation : "")};
   p {
     font-family: "Hyundai Sans Text KR";
     font-size: 16px;
