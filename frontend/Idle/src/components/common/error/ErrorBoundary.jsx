08bcbc3aab945d9df1a43799405a62226aa85876
jjun990908 <jjun990908@naver.com>
2023-08-22 15:17:42 +0900

    [fix] build 오류 수정

--- frontend/Idle/src/components/common/error/ErrorBoundary.jsx
+++ frontend/Idle/src/components/common/error/ErrorBoundary.jsx
@@ -1,5 +1,5 @@
 import { Component } from 'react';
-import ServerErrorPage from '../../../pages/ServerErrorPage';
+import ServerErrorPage from '../../../pages/serverErrorPage';
 
 class ErrorBoundary extends Component {
     constructor(props) {
