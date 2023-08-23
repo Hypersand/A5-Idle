9d7690fb1baa79ac5ec01189c9a829618e970eb7
Hypersand <on8214@gmail.com>
2023-08-23 13:25:24 +0900

    [refactor] 옵션 정렬 메서드 삭제 및 테스트 코드 수정

--- backend/src/main/java/com/autoever/idle/domain/option/service/OptionService.java
+++ backend/src/main/java/com/autoever/idle/domain/option/service/OptionService.java
@@ -14,7 +14,6 @@ import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class OptionService {
-    private static final String NEW = "NEW";
     private final OptionRepository optionRepository;
     private final FunctionRepository functionRepository;
 
@@ -32,56 +31,6 @@ public class OptionService {
             );
         }
 
-        sortByPurchaseRateAndName(optionFunctionsResponseList);
         return optionFunctionsResponseList;
     }
-
-    private void sortByPurchaseRateAndName(List<OptionFunctionsResponse> optionFunctions) {
-        optionFunctions.sort((o1, o2) -> {
-            if (o1.getOptionPrice().equals(o2.getOptionPrice())) {
-                return sortWhenSameOptionPrice(o1, o2);
-            }
-            if (o1.getOptionPrice() < o2.getOptionPrice()) {
-                return -1;
-            }
-            return 1;
-        });
-    }
-
-    private int sortWhenSameOptionPrice(OptionFunctionsResponse o1, OptionFunctionsResponse o2) {
-        int comparedNewOptions = compareIfNewOptions(o1, o2);
-        if (comparedNewOptions != 0) {
-            return comparedNewOptions;
-        }
-
-        return compareRatesAndNames(o1, o2);
-    }
-
-    private int compareIfNewOptions(OptionFunctionsResponse o1, OptionFunctionsResponse o2) {
-        return o1.getOptionPurchaseRate().equals(NEW) ? -1 :
-                o2.getOptionPurchaseRate().equals(NEW) ? 1 : 0;
-    }
-
-    private int compareRatesAndNames(OptionFunctionsResponse o1, OptionFunctionsResponse o2) {
-                double rate1 = extractRate(o1.getOptionPurchaseRate());
-                double rate2 = extractRate(o2.getOptionPurchaseRate());
-
-                if (rate1 == rate2) {
-                    return o2.getOptionName().compareTo(o1.getOptionName());
-                }
-                if (rate1 < rate2) {
-                    return 1;
-        }
-
-        return -1;
-    }
-
-    private double extractRate(String purchaseRate) {
-        String[] splitRate = purchaseRate.split("%")[0].split(" ");
-        if (splitRate.length > 1) {
-            return Double.parseDouble(splitRate[1]);
-        }
-        return 0;
-    }
-
 }
