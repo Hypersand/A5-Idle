4fb0c282bc3919ad1ffb0230ef6949159ec86d54
sue06004 <64758888+sue06004@users.noreply.github.com>
2023-08-23 11:17:09 +0900

    [feat] purchase_rate타입 int로 변경 및 데이터 수정

--- backend/src/main/resources/schema.sql
+++ backend/src/main/resources/schema.sql
@@ -67,7 +67,7 @@ create table if not exists  TRIM
     price         int         null,
     img_url       text        null,
     description   text        null,
-    purchase_rate varchar(20) null,
+    purchase_rate int null,
     car_type_id   bigint      null,
     constraint FK_CAR_TYPE_TO_TRIM_1
     foreign key (car_type_id) references CAR_TYPE (car_type_id)
@@ -84,7 +84,7 @@ create table if not exists  ENGINE
     max_torque    double      null,
     min_fuel      double      null,
     max_fuel      double      null,
-    purchase_rate varchar(20) null,
+    purchase_rate int null,
     description   text        null
     );
 
@@ -108,7 +108,7 @@ create table if not exists  DRIVING_METHOD
     price             int         null,
     description       text        null,
     img_url           text        null,
-    purchase_rate     varchar(20) null
+    purchase_rate     int null
     );
 
 create table if not exists  TRIM_DRIVING_METHOD
@@ -130,7 +130,7 @@ create table if not exists  BODY_TYPE
     type          varchar(10) null,
     price         int         null,
     description   text        null,
-    purchase_rate varchar(20) null,
+    purchase_rate int null,
     img_url       text        null
     );
 
@@ -153,7 +153,7 @@ create table if not exists  EXTERIOR_COLOR
     color             varchar(20) null,
     color_img_url     text        null,
     price             int         null,
-    purchase_rate     varchar(20) null
+    purchase_rate     int null
     );
 
 create table if not exists  CAR_EXTERIOR_IMAGE
@@ -187,7 +187,7 @@ create table if not exists  INTERIOR_COLOR
     price                  int         null,
     interior_img_url       text        null,
     color_img_url          text        null,
-    purchase_rate          varchar(20) null,
+    purchase_rate          int null,
     trim_exterior_color_id bigint      null,
     constraint FK_TRIM_EXTERIOR_COLOR_TO_INTERIOR_COLOR_1
     foreign key (trim_exterior_color_id) references TRIM_EXTERIOR_COLOR (trim_exterior_color_id)
@@ -207,7 +207,7 @@ create table if not exists  `OPTION`
     name               varchar(30) null,
     price              int         null,
     description        text        null,
-    purchase_rate      varchar(20) null,
+    purchase_rate      int null,
     option_category_id bigint      null,
     constraint FK_OPTION_CATEGORY_TO_OPTION_1
     foreign key (option_category_id) references OPTION_CATEGORY (option_category_id)
